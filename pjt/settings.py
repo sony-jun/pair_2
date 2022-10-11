452a95333cc4b405bfdedbbfa15d1f56845ed39f
sony-jun <warrise@naver.com>
2022-10-11 17:29:09 +0900

    회원가입추가

--- pjt/settings.py
+++ pjt/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "review",
+    "accounts",
     "django_bootstrap5",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"

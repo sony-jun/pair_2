2fcd3108253ba78c832fea1e0c58b7dc2754a833
sony-jun <warrise@naver.com>
2022-10-20 17:22:04 +0900

    로그인시 삭제

--- articles/views.py
+++ articles/views.py
@@ -30,6 +30,7 @@ def create(request):
     return render(request, "articles/form.html", context=context)
 
 
+@login_required
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
     comment_form = CommentForm()
@@ -65,6 +66,7 @@ def update(request, pk):
     return render(request, "articles/form.html", context)
 
 
+@login_required
 def delete(request, pk):
     Article.objects.get(id=pk).delete()
     return redirect("articles:index")
@@ -82,7 +84,8 @@ def comment_create(request, pk):
     return redirect("articles:detail", article.pk)
 
 
+@login_required
 def comments_delete(request, article_pk, comment_pk):
     comment = Comment.objects.get(pk=comment_pk)
     comment.delete()
-    return redirect("articles:detail", article_pk)
+    return redirect("articles:detail", article_pk, comment_pk)
